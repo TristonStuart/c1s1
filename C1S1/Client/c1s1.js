d0cb5a1a5b404e67326962a523040c8f67ffc861
Triston Stuart <triston.stuart@icloud.com>
2017-10-14 23:00:02 -0700

    Version : 1.0.3
     + Logging
     - Comments (Most)
     + Future Redirect Handling

--- C1S1/Client/c1s1.js
+++ C1S1/Client/c1s1.js
@@ -1,18 +1,45 @@
 // Notes :
-// Will add comments to make more readable soon
-// Console.logs may be removed in the next version although usefull for debuging (may add debug property in query that will allow or dissallow logs)
+// Added some logging + A logging system. (No way to handle logs as of yet.)
 // Comments will be removed in official release
 // Do not use source code in applications, use obbfuscated version to help prevent tampering. (Also obfuscate your query)
 
 // Load, main function, query is how to handle everything, check api file (/api/query.txt)
 function load(query){
 
+    // Non-Important Variables (Help default processes)
+    let isredirecting = false;
+
+
+    // Logging (FINALLY!) | Note : Logs wont be explained
+    let logs = {}; // Store Logs
+    let sst = new Date(); // System Start Time
+    function getLogTime(){
+
+        let a = new Date(); // Get Current Date
+        let b = a.getTime(); // Get Current Milliseconds since 1970
+        let c = sst.getTime(); // Get sst Milliseconds since 1970
+
+        return String(b - c); // Accurate Logging Time
+
+    }
+    function log(x){
+
+        if (query.doLogs){ // Should Client Log?
+
+            logs[getLogTime()] = x; // Log
+
+        }
+
+    }
+
     console.log('C1S1 | Client - Side Javascript to Server Communicator') // Yay! it works
-    console.log('C1S1 | Client Version : 1.0.2') // May add a version checker in the future
+    console.log('C1S1 | Client Version : 1.0.3') // May add a version checker in the future
+    log('Client Loadded, Version : 1.0.3');
 
     // Generate a ccid, read about the ccids in (/api/ccid.txt)
     function generateCCID() {
 
+        log('Generating CCID')
         var d = new Date().getTime();
 
         var ccid = 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -21,20 +48,23 @@ function load(query){
             return (c=='x' ? r : (r&0x3|0x8)).toString(16);
         });
 
-
+        log('CCID : ' + ccid);
         return ccid;
 
     }
 
     // Set ccid
+    log('Calling to generated CCID');
     let ccid = generateCCID();
 
     // Set server
+    log('Calling to connect to server : ws://' + quer.server);
     let server = new WebSocket('ws://' + query.server);
 
     // Send ip and ccid
     function identify(){
 
+        log('Identifying...');
         // Basicly an object that the server can read
         let packet = {
 
@@ -48,9 +78,11 @@ function load(query){
 
     server.onmessage = function(e){ // Handle Messages recived from server
 
+        log('Client recieved a message.')
         let data = JSON.parse(e.data); // Get the data into a readable format (Object)
         if (data.event) { // Make sure the data exists, idk why it wouldn't
 
+            log('Message Event : ' + data.event);
             if (data.event == "identify"){ // If its identify, process
 
                 if (data.try == "success"){ // If identification successfull
@@ -78,21 +110,24 @@ function load(query){
 
                 if (query.event.redirect == "c1s1"){// Handle by c1s1
 
+                    isredirecting = true;
                     server.close();
                     server = new WebSocket("ws://" + data.redirect.to)
 
                 }else if (query.event.redirect == "c1s1 & query"){// Handle by c1s1 and query
 
+                    isredirecting = true;
                     query.handle.redirect(server, data.redirect)
                     server.close();
                     server = new WebSocket("ws://" + data.redirect.to)
 
                 }else if (query.event.redirect == "query"){// Handle by just query
 
+                    isredirecting = true;
                     query.handle.redirect(server, data.redirect)
 
                 }else {
-                    alert("FATAL ERROR | C1S1 Code : X0222 | Message Dev : " + query.dev.contact) //Fatal Error, will add handle, will add error code lookup too
+                    alert("FATAL ERROR | C1S1 Code : A1 | Message Dev : " + query.dev.contact) //Fatal Error, will add handle, will add error code lookup too
                 }
 
             }else if (data.event == "pong"){// Recieve a pong
@@ -112,6 +147,7 @@ function load(query){
 
     server.onopen = function(){ // When server and client connect
 
+        log('Client connected to server')
         console.log('C1S1 | Connected to server'); // Logs
         if (query.handle.connect){ // If query handle, handle it
 
@@ -127,13 +163,23 @@ function load(query){
 
     server.onclose = function(){ // When server and client disconnect
 
-        if (query.handle.close){ // If query handle, handle
+        log('Disconnecting from server')
+        // Check if redirecting...
+        if (isredirecting){
 
-            query.handle.close(server);
+            log('Disconnect issued as a redirect.')
+
+        }else {
+
+            if (query.handle.close){ // If query handle, handle
+
+                query.handle.close(server);
+
+            }
+            console.log('C1S1 | Stopping Processes, Conenction Closed'); //Logs
+            clearInterval(constPing); // Stop constant pinging of server
 
         }
-        console.log('C1S1 | Stopping Processes, Conenction Closed'); //Logs
-        clearInterval(constPing); // Stop constant pinging of server
 
     }
 
